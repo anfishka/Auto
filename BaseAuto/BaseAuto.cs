6699ad8f98e51b275229fe19300b220c67c3b035
Hanna Borodina <venera131916@gmail.com>
2024-01-15 13:00:22 +0200

    Добавьте файлы проекта.

---
+++ BaseAuto/BaseAuto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Описать класс -Авто- (марка - модель - обьем - привод - коробка). 
+ * Создать список машин и вывести с него только те,
+ * у которых в имени есть буква -а- или -А-
+*/
+
+namespace Auto.BaseAuto
+{
+    internal class BaseAuto
+    {
+       public string Brand;
+       public string Model;
+       public decimal EngineCapacity;
+       public string DriveType;
+       public string Transmission;
+
+        public BaseAuto(string brand, string model, decimal engineCapacity, string driveType, string transmission)
+                {
+                Brand = brand;
+                Model = model;
+                EngineCapacity = engineCapacity;
+                DriveType = driveType;
+                Transmission = transmission;
+                }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Auto.BaseAuto;
+using System.Collections.Generic;
+
+List <string> auto = new List<string>() { "Toyota", "Ford", "Honda", "Volkswagen", "BMW", "Nissan", "Hyundai" };
+
+
+// auto.Where(brand => brand.Contains("a")).ToList().ForEach(i => Console.Write($"{i},"));
+List<string> filteredAuto = auto.Where(brand => brand.Contains("a")).ToList();
+Console.WriteLine($"With string : { string.Join(", ", filteredAuto) } \n");
+
+
+List<BaseAuto> cars = new List<BaseAuto>()
+{
+    new BaseAuto ("Toyota", "Camry", 2.5m, "FWD", "Automatic"),
+    new BaseAuto ("Honda", "Accord", 2.5m, "FWD", "Manual"),
+    new BaseAuto ("Ford", "Accord", 2.5m, "AWD", "Automatic"),
+    new BaseAuto ("Volkswagen", "Golf", 2.5m, "FWD", "Automatic"),
+    new BaseAuto ("BMW", "X5", 3.0m, "AWD", "Automatic"),
+    new BaseAuto ("Nissan", "Altima", 2.5m, "FWD", "CVT"),
+    new BaseAuto ("Hyundai", "Elantra", 2.0m, "FWD", "Automatic"),
+};
+
+ List<BaseAuto> filteredCars = cars.Where(car => car.Brand.Contains("a")).ToList();
+//filteredCars.ForEach(e => Console.Write($"{e.Brand}, "));
+Console.WriteLine($"With class : {string.Join(", ", filteredCars.Select(e => e.Brand))}" );
+
+
+
+
+
